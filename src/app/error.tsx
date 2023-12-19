23ef132d5854b7ec0ae47e80e94b078d508aa2e3
jhonathanalencar <jhonathan.alencarfs@gmail.com>
2023-12-19 16:02:47 -0300

    feat: add error boundary

---
+++ src/app/error.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+import { useEffect } from 'react';
+import Image from 'next/image';
+
+import { AppLayout } from '@shared/modules/layouts/app.layout';
+
+import serverDown from '@assets/images/undraw-server-down.svg';
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+
+  return (
+    <AppLayout>
+      <section className="container mx-auto grid h-full grid-cols-1 place-content-center gap-2 p-2 md:grid-cols-2 ">
+        <div className="mx-auto w-3/4 sm:w-2/3 md:w-full">
+          <Image alt="Server Down" src={serverDown} priority />
+        </div>
+        <div className="flex flex-col items-center justify-center gap-2 pt-6 md:items-start md:place-self-center md:pt-0">
+          <h1 className="text-2xl font-black tracking-wide md:text-4xl">
+            Oops!
+          </h1>
+          <h2 className="text-xl font-semibold tracking-wide text-[#00BFA6] md:text-2xl">
+            Something went wrong!
+          </h2>
+          <button
+            onClick={() => reset()}
+            className="h-12 w-36 rounded bg-yellow-400 text-lg  font-black capitalize text-zinc-800 transition-colors hover:bg-yellow-500 focus-visible:bg-yellow-500 md:h-16 md:w-48 md:text-xl"
+          >
+            Try again
+          </button>
+        </div>
+      </section>
+    </AppLayout>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,11 @@ export const metadata: Metadata = {
     "Welcome to Storesy, where every click unveils a world of possibilities! Storesy is not just an e-commerce platform; it's your gateway to an immersive shopping experience. Discover a curated collection of fashion, electronics, home decor, and more, meticulously handpicked to cater to your unique tastes and preferences.",
 };
 
-export default function AppLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en" className="h-full">
       <body className={`${merriweather_sans.className} h-full`}>
