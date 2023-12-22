5c8909b86c9a239e934bd203759d1112579846ff
jhonathanalencar <jhonathan.alencarfs@gmail.com>
2023-12-22 15:30:36 -0300

    style: customize buttons

--- src/subdomains/products/interfaces/product-details.interface.tsx
+++ src/subdomains/products/interfaces/product-details.interface.tsx
@@ -56,7 +56,7 @@ export function ProductDetailsInterface({
           <p>{product.description}</p>
         </div>
 
-        <div className="product-actions flex w-full flex-col bg-zinc-800 p-2">
+        <div className="product-actions flex h-fit w-full  flex-col rounded-lg bg-zinc-800 p-2 shadow-md md:file:p-4">
           <span className="text-lg font-black tracking-tight text-zinc-200">
             {formatPrice(price)}
           </span>
@@ -64,8 +64,14 @@ export function ProductDetailsInterface({
             quantity_available={product.quantity_available}
           />
           <QuantitySelect quantityAvailable={product.quantity_available} />
-          <button>add to Cart</button>
-          <button>Buy Now</button>
+          <div className="mb-2 mt-4 flex flex-col gap-2">
+            <button className="mx-auto w-4/5 rounded bg-violet-700 py-2 font-semibold text-zinc-200 transition-colors hover:bg-violet-800 md:w-52">
+              Add to Cart
+            </button>
+            <button className="mx-auto w-4/5 rounded bg-yellow-600 py-2 font-semibold text-zinc-200 transition-colors hover:bg-yellow-700 md:w-52">
+              Buy Now
+            </button>
+          </div>
         </div>
       </div>
       <div className="h-0.5 w-full bg-slate-900" />
