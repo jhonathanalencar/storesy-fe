5e2e0308ab851b761cd05242db37ac9033587daf
jhonathanalencar <jhonathan.alencarfs@gmail.com>
2023-12-26 20:55:34 -0300

    feat: add signin and cart links

--- src/shared/modules/components/navbar.component.tsx
+++ src/shared/modules/components/navbar.component.tsx
@@ -1,8 +1,13 @@
 import Link from 'next/link';
+import {
+  MagnifyingGlassIcon,
+  UserIcon,
+  ShoppingCartIcon,
+} from '@heroicons/react/24/solid';
 
 export function Navbar() {
   return (
-    <header className="h-16 bg-zinc-950 px-4 py-1">
+    <header className="h-16 bg-zinc-950 px-4 py-2 shadow-md">
       <div className="navbar-content-grid mx-auto grid h-full w-full max-w-5xl items-center gap-2">
         <div className="navbar-logo h-full">
           <Link
@@ -14,14 +19,38 @@ export function Navbar() {
             </span>
           </Link>
         </div>
-        <div className="navbar-search">
-          <form className="">
-            <input type="text" placeholder="Search" className="w-full" />
+
+        <div className="navbar-search h-full">
+          <form className="flex h-full rounded bg-zinc-800 focus-within:ring-2 focus-within:ring-red-500 focus-within:ring-offset-2 focus-within:ring-offset-zinc-950">
+            <input
+              type="text"
+              placeholder="Search"
+              className="block h-full w-full bg-transparent p-2 text-zinc-400 outline-none"
+            />
+            <button
+              type="submit"
+              className="flex w-12 items-center justify-center rounded-br rounded-tr bg-green-500 transition-colors hover:bg-green-600 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-red-500"
+            >
+              <MagnifyingGlassIcon className="h-6 w-6 text-zinc-900" />
+            </button>
           </form>
         </div>
-        <nav className="navbar-nav bg-blue-200">
-          <Link href="/signin">Sign in</Link>
-          <Link href="/cart">Cart</Link>
+
+        <nav className="navbar-nav flex h-full gap-2">
+          <Link
+            href="/signin"
+            className="inline-flex h-full items-center rounded-sm px-2 text-lg text-zinc-200 outline-1 hover:outline focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-red-500 focus-visible:ring-offset-2 focus-visible:ring-offset-zinc-950"
+          >
+            <UserIcon className="h-6 w-6 text-green-500" />
+            <span className="self-end text-sm">Sign in</span>
+          </Link>
+          <Link
+            href="/cart"
+            className="inline-flex h-full items-center rounded-sm px-2 text-lg text-zinc-200 outline-1 hover:outline focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-red-500 focus-visible:ring-offset-2 focus-visible:ring-offset-zinc-950"
+          >
+            <ShoppingCartIcon className="h-6 w-6 text-green-500" />
+            <span className="self-end text-sm">Cart</span>
+          </Link>
         </nav>
       </div>
     </header>
